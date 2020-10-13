f7ab2ac26d2ebd12cac835389bd6f39204b00c77
mamoru <2827727590@qq.com>
2020-10-13 21:25:55 +0800

    test success

--- testdemo/src/main/java/com/itqf/eight/TestDemoApplication.java
+++ testdemo/src/main/java/com/itqf/eight/TestDemoApplication.java
@@ -28,6 +28,7 @@ package com.itqf.eight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
