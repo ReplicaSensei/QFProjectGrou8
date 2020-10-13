0092040618f83c702201683f0133b96a885d16e5
mamoru <2827727590@qq.com>
2020-10-13 14:20:21 +0800

    springcloud

--- eureka7001/src/main/java/com/itqf/eight/EurekaServerApplication.java
+++ eureka7001/src/main/java/com/itqf/eight/EurekaServerApplication.java
@@ -28,6 +28,7 @@ package com.itqf.eight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
